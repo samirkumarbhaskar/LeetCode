0d4cd6589b405fe81303afcd8c8d9a29b93f0c6f
samirkumarbhaskar <samirkumarbhaskar@gmail.com>
2023-01-31 21:56:07 +0530

    Addition

---
+++ Addition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    cout << "enter the value of a" << endl;
+    cin >> a;
+    cout << "enter the value of b" << endl;
+    cin >> b;
+    cout << "a + b is " << a + b << endl;
+    cout << "a - b is " << a - b << endl;
+    cout << "a * b is " << a * b << endl;
+    cout << "a / b is " << (float)a / b << endl;
+    return 0;
+}
